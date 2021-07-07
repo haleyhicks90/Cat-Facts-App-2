9454c78746c440c0f943454dcd2c2c138f2036f0
Haley Hicks <haleyhicks90@yahoo.com>
2021-07-07 16:55:17 -0400

    added event listeners for carousel buttons

--- cat-facts-app/app.js
+++ cat-facts-app/app.js
@@ -44,10 +44,34 @@ $.ajax({
 
 
 // start of carousel container
-// let currentImgIndex = 0
-// let numOfImages = $(".carousel-images").children().length - 1
+let currentImgIndex = 0
+let numOfImages = $(".carousel-images").children().length - 1
 
+// below: event listener for next button
+$(".next-button").on("click", () => {
+    $(".carousel-images").children().eq(currentImgIndex).css("display", "none")
+    if(currentImgIndex < numOfImages) {
+        currentImgIndex++
+    } else {
+        currentImgIndex = 0
+    }
 
+    $(".carousel-images").children().eq(currentImgIndex).css("display", "block")
+
+})
+
+// below: event listener for previous button
+$(".previous-button").on("click", () => {
+    $(".carousel-images").children().eq(currentImgIndex).css("display", "none")
+    if(currentImgIndex > 0) {
+        currentImgIndex--
+    } else {
+        currentImgIndex = numOfImages
+    }
+
+    $(".carousel-images").children().eq(currentImgIndex).css("display", "block")
+    
+})
 
 // below: closing brackets for window.onload
 })
